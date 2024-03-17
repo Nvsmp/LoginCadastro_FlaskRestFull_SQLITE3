6153b2991a84915acdf71356cc1b5d751e5851c6
Nvsmp <oldblack26@gmail.com>
2024-03-16 23:16:27 -0300

    commit 2

--- Client.py
+++ Client.py
@@ -4,13 +4,13 @@ import requests
 
 resposta = input("[1]LOGIN, \n[2]CADASTRAR\n: ")
 
-if resposta.upper() == "1": 
+if resposta == "1": 
     login = input("LOGIN : ")
     senha = input("SENHA : ")
     resposta = requests.get(f"{config('link_api_principal')}/{config('link_api_3')}/{login},{senha}")
     print(resposta.content)
 
-elif resposta.upper() == "2":
+elif resposta == "2":
     login = input("LOGIN : ")
     senha = input("SENHA : ")
     resposta = requests.post(f"{config('link_api_principal')}/{config('link_api_3')}/{login},{senha}")
